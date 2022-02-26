d23eaa552a539a6839a1c55f98e0afa39aec1181
Christian Collins <99655989+Dabluew@users.noreply.github.com>
2022-02-26 18:16:06 -0500

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,32 @@
+﻿{
+	"version": 1645917191,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/titlebackground.png",
+		"images/title-sheet0.png",
+		"images/playbutton-sheet0.png",
+		"images/ship-sheet0.png",
+		"images/gamebackground.png",
+		"images/health-sheet0.png",
+		"images/beam-sheet0.png",
+		"images/beamparticles.png",
+		"images/enemy-sheet0.png",
+		"images/spawner-sheet0.png",
+		"images/sparks.png",
+		"images/backbutton-sheet0.png",
+		"images/settingsbutton-sheet0.png",
+		"images/tintedteagames-sheet0.png",
+		"images/particles.png",
+		"images/hitbox-sheet0.png",
+		"media/intro.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
